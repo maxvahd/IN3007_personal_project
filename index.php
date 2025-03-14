5b1b6a6705fdca6a845b2166fc586d3f919e57e7
maxvahdat <Max.Vahdat-Khah@city.ac.uk>
2025-03-14 12:57:01 +0000

    Start of Project
    Initial file setup and changes to frontend of webpage

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Discover a plethora of Iranian literature">
+    <meta name="keywords" content="culture, history, polictics, books, iranian">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Satrap</title>
+</head>
+<body>
+    <nav>
+        <div class="title">Satrap</div>
+        <div class="links">
+            <a data-active="index" href="index.php">Home</a>
+            <a data-active="about" href="index.php">About</a>
+            <a data-active="contact" href="index.php">Contact</a>
+        </div>
+    </nav>
+    <header></header>
+    <main></main>
+    <footer></footer>
+    <script src="js/script.js"></script>
+</body>
+</html>
